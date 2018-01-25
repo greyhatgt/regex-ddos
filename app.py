01e23f04f61e51612775f62e6d7cdc73d86b9621
Andrew Ray <aray40@gatech.edu>
2018-01-25 17:31:18 -0500

    Introduce Redis

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+import os
+import redis
 from flask import Flask
+
 app = Flask(__name__)
+host = os.environ.get('REDIS_HOST')
+port = os.environ.get('REDIS_PORT')
+password = os.environ.get('REDIS_PW')
+db = redis.Redis(
+    host=host,
+    port=port,
+    password=password
+)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    foo = db.get('foo')
+    if not foo:
+        db.set('foo', 'bar')
+    return "Hello, %s!" % str(foo)
 
 if __name__ == '__main__':
     app.run()
